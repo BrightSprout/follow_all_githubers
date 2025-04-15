4df9c6adbe8072333ae39e6efc0ff4143a13ca35
BrightSprout <brightsprout0@gmail.com>
2025-04-15 11:46:01 +0800

    feat: loop through every github users

--- script.sh
+++ script.sh
@@ -18,9 +18,6 @@ get_users() {
     fi
 }
 
-# retrieve list of users
-get_users
-
 follow_user() {
    url="https://api.github.com/user/following/${1}"
    auth_key=$(cat auth_key.txt)
@@ -34,38 +31,52 @@ follow_user() {
 
    http_code="${response: -3}"
 
-   if [ "$http_code" -ne 200 ]; then
-       users_failed_follow+=("${1}")
-       echo "Failed to follow: \"${1}\""
-   else 
+   if [ "$http_code" -eq 200 ] || [ "$http_code" -eq 204 ]; then
        followed_users+=("${1}")
        echo "Followed: \"${1}\""
+   else 
+       users_failed_follow+=("${1}")
+       echo "Failed to follow: \"${1}\""
    fi
 }
 
-# loop through array of users encoded in base64
-for user in $(echo "$users"); do
-   # decode the property of json in base64
-   _jq() {
-       echo "$user" | base64 --decode | jq -r ${1}
-   }
+clean_up() {
+    # update the last since_id
+    echo "$since_id" > since.txt
+        
+    # store the users that we haven't able to follow
+    IFS=" "
+    users_failed_follow_str="${users_failed_follow[*]}"
+    unset IFS
+    
+    echo "$users_failed_follow_str" >> users_failed_follow.txt
+    
+    # store the users we have followed
+    IFS=" "
+    users_failed_follow_str="${followed_users[*]}"
+    unset IFS
+    
+    echo "$users_failed_follow_str" >> users_followed.txt
+}
 
-  username=$(_jq '.login')
-  
-  # follow the user with given username
-  follow_user "$username"
+trap clean_up EXIT
+
+while true; do
+    # retrieve list of users
+    get_users
+    
+    # loop through array of users encoded in base64
+    for user in $users; do
+       # decode the property of json in base64
+       _jq() {
+           echo "$user" | base64 --decode | jq -r ${1}
+       }
+    
+      username=$(_jq '.login')
+      
+      # follow the user with given username
+      follow_user "$username"
+
+      since_id=$(_jq '.id')
+    done
 done
-
-# store the users that we haven't able to follow
-IFS=" "
-users_failed_follow_str="${users_failed_follow[*]}"
-unset IFS
-
-echo "$users_failed_follow_str" >> users_failed_follow.txt
-
-# store the users we have followed
-IFS=" "
-users_failed_follow_str="${followed_users[*]}"
-unset IFS
-
-echo "$users_failed_follow_str" >> users_followed.txt
